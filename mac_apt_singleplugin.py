388e0e54bfccb0836a59f2cf0378368897b47102
Yogesh Khatri (@swiftforensics) <yogesh@swiftforensics.com>
2019-06-23 12:37:15 -0400

    upped ver for singleplugin

--- mac_apt_singleplugin.py
+++ mac_apt_singleplugin.py
@@ -29,7 +29,7 @@ import time
 import textwrap
 from plugin import *
 
-__VERSION = "0.3"
+__VERSION = "0.4"
 __PROGRAMNAME = "macOS Artifact Parsing Tool - Single Plugin mode"
 __EMAIL = "yogesh@swiftforensics.com"
 
