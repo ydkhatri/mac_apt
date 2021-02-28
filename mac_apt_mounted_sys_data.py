eb209effab12a1c660047863732bc82a62aaa570
ydkhatri <yogesh@swiftforensics.com>
2021-02-28 11:33:34 -0500

    Version 1.1

--- mac_apt_mounted_sys_data.py
+++ mac_apt_mounted_sys_data.py
@@ -32,7 +32,7 @@ from plugins.helpers.writer import *
 from plugins.helpers.disk_report import *
 from plugin import *
 
-__VERSION = "1.0"
+__VERSION = "1.1"
 __PROGRAMNAME = "macOS Artifact Parsing Tool - SYS DATA Mounted mode"
 __EMAIL = "yogesh@swiftforensics.com"
 
